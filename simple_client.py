397aeeb0c694c74f57d8f502bfd103e2781d4672
Filip Byrén <34345786+FilipByren@users.noreply.github.com>
2018-08-17 12:12:34 +0200

    Add files via upload

--- simple_client.py
+++ simple_client.py
@@ -3,17 +3,16 @@ import tensorflow as tf
 
 # This is a placeholder for a Google-internal import.
 
-from grpc.beta import implementations
+import grpc
 from google.protobuf import json_format, text_format
 
 from tensorflow_serving.apis import predict_pb2
-from tensorflow_serving.apis import prediction_service_pb2
+from tensorflow_serving.apis import prediction_service_pb2_grpc
 
 from absl import flags
 from absl import app
 flags.DEFINE_string('server', 'localhost:9000',
                            'PredictionService host:port')
-flags.DEFINE_string('data','', 'Word')
 flags.DEFINE_string('model_name','saved', 'Word')
 flags.DEFINE_string('signature_name','serving_default', 'Word')
 FLAGS = flags.FLAGS
@@ -22,8 +21,8 @@ FLAGS = flags.FLAGS
 
 def main(_):
 
-  channel = implementations.insecure_channel(FLAGS.server)
-  stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+  channel = grpc.insecure_channel(FLAGS.server)
+  stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
   request = predict_pb2.PredictRequest()
   request.model_spec.name = FLAGS.model_name
   request.model_spec.signature_name = FLAGS.signature_name
