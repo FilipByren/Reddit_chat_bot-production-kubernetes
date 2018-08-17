dec3fe545239129995b36fc3153f211200700a6d
Filip Byrén <fbyren@kth.se>
2018-08-17 09:45:25 +0200

    Fix client

--- seq_seq_client.py
+++ seq_seq_client.py
@@ -30,20 +30,19 @@ def main(_):
   channel = grpc.insecure_channel(FLAGS.server)
   stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
   # Send request
-  with open(FLAGS.data, 'rb') as f:
-    # See prediction_service.proto for gRPC request/response details.
-    encode_test_input = data_utils.encode([f.read()],w2idx)
-    request = predict_pb2.PredictRequest()
-    request.inputs.CopyFrom(tf.contrib.util.make_tensor_proto(encode_test_input))
-    output = stub.Predict(request, 10.0)  # 10 secs timeout
-    replies = []
-    for ii, oi in zip(encode_test_input, output):
-        q = data_utils.decode(sequence=ii, lookup=metadata['idx2w'], separator=' ')
-        decoded = data_utils.decode(sequence=oi, lookup=metadata['idx2w'], separator=' ').split(' ')
-        if decoded.count('unk') == 0:
-            #if decoded not in replies:
-            print('q : [{0}]; a : [{1}]'.format(q, ' '.join(decoded)))
-            replies.append(decoded)
+  # See prediction_service.proto for gRPC request/response details.
+  encode_test_input = data_utils.encode([FLAGS.data],w2idx)
+  request = predict_pb2.PredictRequest()
+  request.inputs.CopyFrom(tf.contrib.util.make_tensor_proto(encode_test_input))
+  output = stub.Predict(request, 10.0)  # 10 secs timeout
+  replies = []
+  for ii, oi in zip(encode_test_input, output):
+      q = data_utils.decode(sequence=ii, lookup=metadata['idx2w'], separator=' ')
+      decoded = data_utils.decode(sequence=oi, lookup=metadata['idx2w'], separator=' ').split(' ')
+      if decoded.count('unk') == 0:
+          #if decoded not in replies:
+          print('q : [{0}]; a : [{1}]'.format(q, ' '.join(decoded)))
+          replies.append(decoded)
 
 
 if __name__ == '__main__':
